ce5e280288502c862223c614aadbf52c77c0e74f
Sam Gildea <sam.c.gildea@gmail.com>
2019-10-20 12:35:21 -0400

    adding build folder

---
+++ build/precache-manifest.6e87931fd66a013adc6237b5274c3b9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9e25c9241a3933700b4079b848a72f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09d0132fdc74ca8f73ac",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1350465c2b0f3562aeab",
+    "url": "/static/css/main.e3396eed.chunk.css"
+  },
+  {
+    "revision": "09d0132fdc74ca8f73ac",
+    "url": "/static/js/2.233a2383.chunk.js"
+  },
+  {
+    "revision": "1350465c2b0f3562aeab",
+    "url": "/static/js/main.ec730412.chunk.js"
+  },
+  {
+    "revision": "67d4bf702f86dd3e6125",
+    "url": "/static/js/runtime-main.de3e7f27.js"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/ProximaNova-Regular.410504d4.otf"
+  }
+]);
